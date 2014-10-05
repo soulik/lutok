89eeed6a1663fc4d49008c565024ab8fad350559
Mário Kašuba <mario.kasuba@it-academy.sk>
2014-10-05 23:51:18 +0200

    Initial commit for a new headers-only version

--- lutok/lutok.hpp
+++
@@ -1,7 +0,0 @@
-#include <lutok/exceptions.hpp>
-#include <lutok/operations.hpp>
-#include <lutok/state.ipp>
-#include <lutok/buffer.hpp>
-#include <lutok/lobject.hpp>
-#include <lutok/stack_cleaner.hpp>
-#include <lutok/debug.hpp>
